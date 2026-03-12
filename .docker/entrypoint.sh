96e3c54126d957d47914fe295064132bfd1a4963
AtumCor <jerrymeng007@gmail.com>
2026-03-12 16:59:24 +0000

    made running docker much much faster, fixed  sizing issue with contact, uji, and webstore page

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 set -e
 
 cd /usr/src/app
@@ -12,24 +12,17 @@ bundle check || bundle install
 
 python3 _cite/cite.py
 
-(
-  watchmedo auto-restart \
-    --debug-force-polling \
-    --patterns="_config.yaml;*.md;*.html;*.yml;*.yaml;*.scss;*.js" \
-    --recursive \
-    --signal SIGTERM \
-    -- \
-    bundle exec jekyll serve \
-      --livereload \
-      --incremental \
-      --trace \
-      --host=0.0.0.0 \
-      --port=4000
-) | sed "s/LiveReload address.*//g;s/0.0.0.0/localhost/g" &
-
 watchmedo shell-command \
-    --debug-force-polling \
-    --recursive \
-    --wait \
-    --command="python3 _cite/cite.py" \
-    --patterns="_data/sources*;_data/orcid*;_data/pubmed*;_data/google-scholar*"
+  --debug-force-polling \
+  --recursive \
+  --wait \
+  --command="python3 _cite/cite.py" \
+  --patterns="_data/sources*;_data/orcid*;_data/pubmed*;_data/google-scholar*" &
+
+exec bundle exec jekyll serve \
+  --livereload \
+  --force_polling \
+  --incremental \
+  --trace \
+  --host=0.0.0.0 \
+  --port=4000
